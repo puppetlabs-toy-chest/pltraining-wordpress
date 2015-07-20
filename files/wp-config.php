86c208ae81313fd925d74d86f661d5c0ec5ba1e9
Carthik Sharma <carthik@puppetlabs.com>
2015-07-20 16:09:34 -0700

    trim to make solution fit on a page

--- files/wp-config.php
+++ files/wp-config.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, and ABSPATH. You can find more information by visiting
- * {@link https://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
- * Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
-
-// ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
 
@@ -52,7 +36,6 @@ define('LOGGED_IN_SALT',   'put your unique phrase here');
 define('NONCE_SALT',       'put your unique phrase here');
 
 /**#@-*/
-
 /**
  * WordPress Database Table prefix.
  *
@@ -63,7 +46,6 @@ $table_prefix  = 'wp_';
 
 /**
  * For developers: WordPress debugging mode.
- *
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
